ab6c43a4fa01bda71ee996c9326bbe63f2080059
abickford <atb5498@gmail.com>
2020-01-27 23:49:49 -0600

    created readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# note-taker
+# Note Taker
+This application is intended to allow users to to create, save, and delete previously made notes.
+
+# User story
+AS A user
+
+I WANT to be able to write notes
+
+SO THAT I can organize my thoughts
+
+# Getting Started
+1. Click the GET STARTED button on the landing page
+2. Create a note title and text within the note
+3. Click the floppy disk icon in the upper-right corner to save the note
+4. All saved notes are displayed in the left column by their note title and in the order in which they were created
+5. Click the trash can icon to delete a saved note
+
+# Link
+https://github.com/atb5498/note-taker
+
+# Built With
+npm: https://www.npmjs.com/
+
+express: https://www.npmjs.com/package/express
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const db = require("./db/db.json");
+const notes = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,20 +14,20 @@ app.get("/notes", (req, res) => {
 })
 
 app.get("/api/notes", (req, res) => {
-    res.json(db);
+    res.json(notes);
 })
 
 app.post("/api/notes", (req, res) => {
     const note = {
-        id: db.length + 1,
+        id: notes.length + 1,
         title: req.body.title,
         text: req.body.text
     }
-    db.push(note);
+    notes.push(note);
     res.sendStatus(200);
 })
 
-app.delete("/api/notes:id", (req, res) => {
+app.delete("/api/notes/:id", (req, res) => {
     res.sendStatus(200);
 })
 
